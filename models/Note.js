ad684ffae51849f7f932810713f728ceb46fa5df
ardato belay <aradatobelay@gmaill.com>
2020-08-30 12:34:39 +0300

    initial commit

---
+++ models/Note.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const NoteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please add a title"],
+      unique: true,
+      trim: true,
+      maxlength: [40, "Title cannot be more then 40 Characters"],
+    },
+    description: {
+      type: String,
+      required: true,
+      maxlength: [200, "Description cannot be more 200 Characters"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.models.Note || mongoose.model("Note", NoteSchema);
